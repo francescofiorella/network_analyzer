1cd072dbaaf7184e69a6eb88b8700cf09cdd5fe0
Antonello Caputo s302879 <s302879@studenti.polito.it>
2022-09-13 16:11:02 +0200

    Error Struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 pub mod sniffer {
+    use std::error::Error;
+    use std::fmt::{Display, Formatter};
+    use pcap::Packet;
+
     pub struct Sniffer {}
-    pub struct Packet {
+    pub struct NAPacket {
         destination_mac_address: String, // 0 - 5
         source_mac_address: String, // 6 - 11
         level_three_type: u16, // 12 - 13
@@ -18,4 +22,24 @@ pub mod sniffer {
         destination_port: u16, // 36 - 37
         other_data: Vec<u8>
     }
+
+
+    #[derive(Debug)]
+    pub struct NAError{
+        message: String,
+    }
+
+    impl NAError{
+        fn new(msg: &str)-> Self{NAError{message: msg.to_string()}}
+    }
+
+    impl Display for NAError{
+        fn fmt(&self, f:&mut Formatter<'_>)-> std::fmt::Result{
+            write!(f,"NAError: {}",self.message)
+        }
+    }
+
+    impl Error for NAError{}
+
+
 }
