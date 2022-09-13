7efe9c0a693f297416535700ed4e825fd7800508
MATTIA CAMPAGNOLA s297404 <s297404@studenti.polito.it>
2022-09-13 18:28:50 +0200

    network adapter configuration (no command-line) avviata

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{Read, stdin, stdout, Write};
+use clap::Parser;
+use network_analyzer::sniffer::{list_adapters, na_config};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    network_adapter: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    //let args = Args::parse();
+    na_config();
+
+
 }
